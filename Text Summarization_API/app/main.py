fd85acab46451091df6bd497b5977c8ccb92f216
Velicia Varel Valiant <100945245+veliciavv@users.noreply.github.com>
2024-12-19 21:55:42 +0800

    Update main.py
    update summary parameter

--- Text Summarization_API/app/main.py
+++ Text Summarization_API/app/main.py
@@ -19,20 +19,12 @@ def summarize(request: SummarizationRequest):
     
     text_lowercase = request.text.lower()
 
-    # Panggil fungsi summarize_text dengan semua parameter
     summary = summarize_text(
         text=text_lowercase,
         max_length=request.max_length,
         min_length=request.min_length,
         no_repeat_ngram_size=request.no_repeat_ngram_size,
         num_beams=request.num_beams,
-        # repetition_penalty=request.repetition_penalty,
-        # length_penalty=request.length_penalty,
-        # early_stopping=request.early_stopping,
-        # do_sample=request.do_sample,
-        # temperature=request.temperature,
-        # top_k=request.top_k,
-        # top_p=request.top_p,
     )
     
     return SummarizationResponse(summary=summary)
